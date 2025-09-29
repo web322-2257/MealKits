35fc230185ce2196db81dbb1c1536ed1bb68377b
Bilal <bumar@myseneca.ca>
2025-09-28 21:37:49 -0400

    Updated layouts

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ const path = require("path");
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
 const mealkitData = require('./modules/mealkit-util');
 
 
 app.set('view engine', 'ejs');
-
+app.use(expressLayouts);
+app.set('layout', 'layouts/main');
 app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files from the "public" folder
@@ -56,10 +58,7 @@ app.get('/headers', (req, res) => {
 
 // This use() will add an error handler function to
 // catch all errors.
-app.use(function (err, req, res, next) {
-    console.error(err.stack)
-    res.status(500).send("Something broke!")
-});
+
 
 // Define a port to listen to requests on.
 const HTTP_PORT = process.env.PORT || 8080;
